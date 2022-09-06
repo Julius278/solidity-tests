05814bf5b12835c730181babd9d07312b57dc979
Remix Workspace <>
2022-09-06 11:04:26 +0200

    break in for loop

--- contracts/loops.sol
+++ contracts/loops.sol
@@ -7,12 +7,23 @@ contract Loops {
     event Number(
         uint date,
         address add,
-        uint number
+        uint number,
+        uint i
     );
 
     function showNumbers(uint number, uint amount) public {
         for(uint i = 0; i< amount; i++){
-            emit Number(block.timestamp, msg.sender, number);
+            emit Number(block.timestamp, msg.sender, number, i);
+        }
+    }
+
+    function showNumbersWithBreak(uint number, uint amount) public {
+        for(uint i = 0; i< amount; i++){
+
+            if(i > 5){
+                break;
+            }
+            emit Number(block.timestamp, msg.sender, number, i);
         }
     }
 }
